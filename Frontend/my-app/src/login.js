377738e6120c7dff7c3aa9dad42b2c5fcb6f3f35
nathanml <nathanml@bu.edu>
2019-11-16 18:56:23 -0500

    fixing login

--- Frontend/my-app/src/login.js
+++ Frontend/my-app/src/login.js
@@ -2,48 +2,61 @@ import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import "./login.css";
 
-export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
+class Login extends React.Component{
+  constructor(props) {
+    super(props);
+    this.validateForm = this.validateForm.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.goToInternalLink = this.goToInternalLink.bind(this)
   }
 
-  function handleSubmit(event) {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  validateForm() {
+    this.setState(this.validateForm)
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
   }
 
-  function goToInternalLink(path){
-      var myDomain = 'localhost:3000';
-      window.location.href = myDomain + path;
+  goToInternalLink(path){
+      var myDomain = 'http://example.com';
+      window.location.href = myDomain + "/" + path;
     }
-
-  return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-        <input type="button" value="GO!" onClick={goToInternalLink('./signup.js')}/>
-      </form>
-    </div>
-  );
+  render(){
+    return (
+      <div className="Login">
+        <form onSubmit={this.state.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={e => this.state.email= e.target.value}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={e => this.state.setPassword(e.target.value)}
+              type="password"
+            />
+          </FormGroup>
+          <Button block bsSize="large" onClick={this.validateForm()} type="submit">
+            Login
+          </Button>
+          <input type="button" value="Go to Signup" onclick={"goToInteralLink('android_asset/home.html')"}/>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default Login

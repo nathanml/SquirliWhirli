4437b6cc1049ba482e5bd822b09194b8dd098087
nathanml <nathanml@bu.edu>
2019-11-16 16:28:19 -0500

    connect SQL

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,7 @@ const express = require("express")
 const app = express()
 const port = 1000
 app.listen(port,()=> console.log("port 1000"))
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+
+})

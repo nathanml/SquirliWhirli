c796c66ff50b15b87e8d617233064e4690a766ee
nathanml <nathanml@bu.edu>
2019-11-16 22:31:51 -0500

    db connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
-const app = express();
 const sql = require("mysql");
 const port = 1000
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'secret',
+    database: 'nodemysql'
+});
+
+db.connect(function(()=> {
+    if(err){
+        throw err;
+    }
+    else{
+        console.log("MySQL connected")
+    }
+})
 const app = express();
+
 app.listen('1000', () =>{
     console.log("Server started on 1000")
-}
+})
 
 app.listen(port,()=> console.log("port 1000"))

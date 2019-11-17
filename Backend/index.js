696cca58fedc33e3ae1ddab07ece01c0191fcee6
nathanml <nathanml@bu.edu>
2019-11-16 23:56:17 -0500

    Connected to the database

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const sql = require("mysql");
+const mysql = require("mysql");
 const port = 1000
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'secret',
-    database: 'nodemysql'
-});
-
-db.connect(function(()=> {
-    if(err){
-        throw err;
-    }
-    else{
-        console.log("MySQL connected")
-    }
-})
 const app = express();
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Password",
+    database:"squirlidb",
+    port: '1000'
+  });
+  
+con.connect();
+console.log("MySQL connected")
+
+con.query('SELECT * FROM doctors', function (error, results, fields) {
+    if (error) throw error;
+    //console.log('The solution is: ', results[0].solution);
+    console.log("DATA FROM DOCTORS")
+  });
 
 app.listen('1000', () =>{
     console.log("Server started on 1000")
 })
-
-app.listen(port,()=> console.log("port 1000"))
+console.log("HELLO!!!")
+con.end();

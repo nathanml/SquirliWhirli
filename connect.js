9009bc076d01d0321836f43e61f2fdf9bfa747f2
nathanml <nathanml@bu.edu>
2019-11-16 21:34:20 -0500

    add SQL script

--- connect.js
+++ connect.js
@@ -1,4 +1,22 @@
-let mysql = require('mysql');
+const express = require("express");
+const app = express();
+const mysql = require("mysql")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+
+//open the database
+
+let dbase = new mysql.Database("", mysql.OPEN_READWRITE, (err) =>{
+	if(err){
+		console.error(err.message)
+
+	}
+console.log("Database accessed")
+})
 
 let connection = mysql.createConnection({
     host: 'localhost',

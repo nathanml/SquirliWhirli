43a6d92761811381fb43a38d07c16dbd890b0e4f
nathanml <nathanml@bu.edu>
2019-11-16 22:18:52 -0500

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const sql = require("mysql");
 const port = 1000
+
+const app = express();
+app.listen('1000', () =>{
+    console.log("Server started on 1000")
+}
+
 app.listen(port,()=> console.log("port 1000"))
--- connect.js
+++ connect.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql")
 const bodyParser = require("body-parser")
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
